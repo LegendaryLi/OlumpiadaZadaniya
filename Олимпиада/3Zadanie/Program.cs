d90ba37be3f7b3d747bdb323506529508a3d23a6
LegendaryLi <93376990+LegendaryLi@users.noreply.github.com>
2021-11-19 14:55:33 +0300

    Add files via upload

---
+++ Олимпиада/3Zadanie/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace OlumpZad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            if (!File.Exists("input.txt"))
+            {
+                File.Create("input.txt").Dispose();
+            }
+            string str = null;
+            bool error = false;
+            while (!sr.EndOfStream) { str += sr.ReadLine(); }
+            sr.Close();
+            if (str == null) { error = true; }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != '1' && str[i] != '0') error = true;
+            }
+            if (error == false)
+            {
+                string temp = null;
+                int n = 0;
+                int count = 0;
+
+                for (int i = 0; i < str.Length / 8; i++)
+                {
+                    temp = str.Substring(n, 8);
+                    if (temp.Substring(0, 2) != "10")
+                        count++;
+                    n += 8;
+                }
+                StreamWriter sw = new StreamWriter("output.txt");
+                if (!File.Exists("output.txt"))
+                {
+                    File.Create("output.txt").Dispose();
+                }
+                sw.WriteLine(count);
+                sw.Close();
+                Console.WriteLine("Программа завершила работу...");
+            }
+            else if (error == true)
+            {
+                Console.WriteLine("Строка содержит недопустимые символы");
+            }
+        }
+    }
+}
---
+++ Олимпиада/4Zadanie/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace OlumpZad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rules = true;
+            while (rules)
+            {
+                ArrayList dates = new ArrayList();
+                StreamReader sr = new StreamReader("input.txt");
+                string nomerofmeteoritstroka = sr.ReadLine();
+                int k;
+                try { k = int.Parse(nomerofmeteoritstroka.Split(" ")[1]); }
+                catch { break; }
+                if (k < 1 || k > Math.Pow(10, 5)) { break; };
+                while (!sr.EndOfStream)
+                {
+                    dates.Add(sr.ReadLine());
+                }
+                dates.Remove(0);
+                try
+                {
+                    for (int i = 0; i < dates.Count; i++)
+                    {
+                        int year = int.Parse(dates[i].ToString().Split(" ")[0].Split("-")[0]);
+                        if (year < 1600 || year > 2500) break;
+                        int month = int.Parse(dates[i].ToString().Split(" ")[0].Split("-")[1]);
+                        if (month < 1 || month > 12) break;
+                        int day = int.Parse(dates[i].ToString().Split(" ")[0].Split("-")[2]);
+                        if (day < 1 || day > 31) break;
+                        int hour = int.Parse(dates[i].ToString().Split(" ")[1].Split(":")[0]);
+                        if (hour < 0 || hour > 23) break;
+                        int minute = int.Parse(dates[i].ToString().Split(" ")[1].Split(":")[1]);
+                        if (minute < 0 || minute > 59) break;
+                        int second = int.Parse(dates[i].ToString().Split(" ")[1].Split(":")[2]);
+                        if (second < 0 || second > 59) break;
+                    }
+                }
+                catch { break; }
+                int n = dates.Count;
+                if (n != int.Parse(nomerofmeteoritstroka.Split(" ")[0])) break;
+                dates.Sort();
+                foreach (object e in dates)
+                {
+                    Console.WriteLine(e);
+                }
+                string neededdate = dates[k - 1].ToString();
+                Console.WriteLine();
+                Console.WriteLine(neededdate);
+                sr.Close();
+                StreamWriter sw = new StreamWriter("output.txt");
+                sw.WriteLine(dates[k - 1].ToString());
+                sw.Close();
+                rules = false;
+                break;
+            }
+            if (rules) Console.WriteLine("Ошибка в данных");
+        }
+    }
+}
